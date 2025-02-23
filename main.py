cde5ab365ea40d45f3b767e18d60faf7e2d1cb5d
Pavle Milanov <pavle_milanov@hotmail.com>
2025-02-23 01:52:38 +0100

    Added gpu acceleration support (linux only)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import List, Dict, Optional, Tuple
 import whisper
 import ffmpeg
 import requests
+import subprocess
 
 def get_video_resolution(video_path:str) -> Optional[Tuple[int, int]]:
 	try:
@@ -25,9 +26,27 @@ def get_subtitles(video_file:str, model:str="small") -> dict:
 def translate_subs(text:str, target_lang:str, source_lang:str="auto") -> str:
 	return requests.get(f"https://lingva.lunar.icu/api/v1/{source_lang}/{target_lang}/{text}").json()["translation"]
 
-def add_subtitles_to_video(video_file:str, output_file:str, subtitles:List[Dict], subtitle_fontsize:int=30, subtitle_color:str="white", box_color:str="black", font:str="LiberationSans", boxborderw:int=5, translate:str=None):
+def check_gpu() -> str:
+	result = subprocess.run(["lspci"], stdout=subprocess.PIPE)
+	gpus = result.stdout.decode().strip().split("\n")
+	for gpu in gpus:
+		if "NVIDIA" in gpu:
+			return "nvidia"
+		elif "AMD" in gpu:
+			return "amd"
+	return None
+
+def add_subtitles_to_video(video_file:str, output_file:str, subtitles:List[Dict], subtitle_fontsize:int=30, subtitle_color:str="white", box_color:str="black", font:str="LiberationSans", boxborderw:int=5, translate:str=None, gpu_acceleration:bool=False):
 	x,y = get_video_resolution(video_file)
-	video_stream = ffmpeg.input(video_file).video
+	if gpu_acceleration != False:
+		gpu = check_gpu()
+		if gpu == "nvidia":
+			hwaccel='cuda'
+		elif gpu == "amd":
+			hwaccel="opencl",
+		video_stream = ffmpeg.input(video_file, hwaccel=hwaccel).video
+	else:
+		video_stream = ffmpeg.input(video_file).video
 	audio_stream = ffmpeg.input(video_file).audio
 	for sub in subtitles["segments"]:
 		text = sub["text"]
@@ -35,7 +54,14 @@ def add_subtitles_to_video(video_file:str, output_file:str, subtitles:List[Dict]
 			text = translate_subs(sub["text"], target_lang=translate)
 		x_position = (x - (subtitle_fontsize * 0.474 * len(text))) / 2
 		video_stream = ffmpeg.drawtext(video_stream, text, x_position, y-70, box=1, boxcolor=box_color, boxborderw=boxborderw ,fontcolor=subtitle_color, fontsize=subtitle_fontsize, enable=f"between(t,{sub['start']},{sub['end']})")
-	stream = ffmpeg.output(video_stream, audio_stream, output_file)
+	if gpu_acceleration != False:
+		if gpu == "nvidia":
+			vcodec = "h264_nvenc"
+		elif gpu == "amd":
+			vcodec="h264_amf"
+		stream = ffmpeg.output(video_stream, audio_stream, output_file, vcodec=vcodec)
+	else:
+		stream = ffmpeg.output(video_stream, audio_stream, output_file)
 	print(stream.compile())
 	stream.run()
 
@@ -43,8 +69,7 @@ def main():
 	vid = "vid_30.mp4"
 	output = "output.mp4"
 	subs = get_subtitles(vid)
-	print(subs["segments"])
-	add_subtitles_to_video(vid, output, subs, translate="sr")
+	add_subtitles_to_video(vid, output, subs, gpu_acceleration=True)
 	print("Subtitles added. Enjoy!")
 
 if __name__ == "__main__":
